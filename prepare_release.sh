ddb7a5f60368ea511e3faded9bf96c81bbf8de1d
hejian08 <hejian08@baidu.com>
2015-09-29 14:43:15 +0800

    update two shell scripts

--- prepare_release.sh
+++ prepare_release.sh
@@ -5,9 +5,6 @@
 # https://cwiki.apache.org/confluence/display/FLUME/How+to+Release
 
 # Prepare branches and create tag
-# In this section, the release is X.Y.Z (e.g. 1.3.0)
-
-# 3. Update the "version" value of all pom.xml and RELEASE-NOTES in trunk to X.(Y+1).0:
 
 X=$1
 Y=$2
@@ -15,30 +12,28 @@ Z=$3
 
 let "ZN=$Z+1"
 
-# 7. Checkout the release branch:
+
 git checkout brance-baidu-waimai
 git add .
 git commit -m "prepare-release"
 
 
+# Checkout  branch, Remove -SNAPSHOT from the release branch and commit
 git checkout -b waimai-branch-$X.$Y.$Z
-
-# 8. Remove -SNAPSHOT from the release branch and commit
 find . -name pom.xml | xargs sed -i "" -e "s/$X.$Y.$Z-SNAPSHOT/$X.$Y.$Z/"
 git add .
 git commit -m "Removing -SNAPSHOT from waimai-branch-$X.$Y.$Z branch"
+git push origin waimai-branch-$X.$Y.$Z
 
+
+# make tag
 git tag -a waimai-tag-$X.$Y.$Z -m "waimai Flume $X.$Y.$Z release."
 git push origin waimai-tag-$X.$Y.$Z
-#git tag -d release-$X.$Y.$Z
-#git push origin :refs/tags/release-$X.$Y.$Z
 
 
-
+# change $X.$Y.$Z-SNAPSHOT to $X.$Y.$ZN-SNAPSHOT
 git checkout brance-baidu-waimai
 find . -name pom.xml | xargs sed -i "" -e "s/$X.$Y.$Z-SNAPSHOT/$X.$Y.$ZN-SNAPSHOT/"
 git add .
 git commit -m "change $X.$Y.$Z-SNAPSHOT to $X.$Y.$ZN-SNAPSHOT"
 git push -u origin brance-baidu-waimai:brance-baidu-waimai
-
-# git checkout waimai-tag-release-$X.$Y.$Z
--- prepare_release_rollback.sh
+++ prepare_release_rollback.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 
-# ./prepare_release.sh 1 7 0
-# https://cwiki.apache.org/confluence/display/FLUME/How+to+Release
-
-# Prepare branches and create tag
-# In this section, the release is X.Y.Z (e.g. 1.3.0)
-
-# 3. Update the "version" value of all pom.xml and RELEASE-NOTES in trunk to X.(Y+1).0:
+# ./prepare_release_rollback.sh 1 7 0 $shaId
+# 回滚 prepare_release
 
 X=$1
 Y=$2
@@ -15,12 +10,18 @@ Z=$3
 
 let "ZN=$Z+1"
 
+git checkout brance-baidu-waimai
+
 
 git branch -D waimai-branch-$X.$Y.$Z
+git push origin :waimai-branch-$X.$Y.$Z
+
 
 git tag -d waimai-tag-$X.$Y.$Z
 git push origin :refs/tags/waimai-tag-$X.$Y.$Z
 
 
+git reset --hard $4
+git push origin HEAD --force
 
-# git checkout waimai-tag-release-$X.$Y.$Z
+
